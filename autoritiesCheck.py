0d245830c1929903429741656854f71133d97586
Toni Hermoso Pulido <toniher@cau.cat>
2025-01-09 16:15:11 +0100

    more carry on

--- autoritiesCheck.py
+++ autoritiesCheck.py
@@ -2,19 +2,27 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import pprint
-import MySQLdb
-import re
-import json
 import csv
 import gzip  # Using gzip file
+import json
+import logging
+import pprint
+import re
+import sys
 
+import MySQLdb
+
+logging.basicConfig(encoding="utf-8", level=logging.INFO)
 pp = pprint.PrettyPrinter(indent=4)
 
-parser = argparse.ArgumentParser(description="""Script for parsing authorities from dumps and DB""")
-parser.add_argument("-dump",help="""Path to Dump file""")
-parser.add_argument("-authorities",help="""Path to Authorities file""")
-parser.add_argument("-config",help="""Path to a JSON file with configuration options!""")
+parser = argparse.ArgumentParser(
+    description="""Script for parsing authorities from dumps and DB"""
+)
+parser.add_argument("-dump", help="""Path to Dump file""")
+parser.add_argument("-authorities", help="""Path to Authorities file""")
+parser.add_argument(
+    "-config", help="""Path to a JSON file with configuration options!"""
+)
 args = parser.parse_args()
 
 data = {}
@@ -32,28 +40,37 @@ if "config" in args:
 if "mysql" in data:
     dbfile = None
     mysqlmode = True
-    conn = MySQLdb.connect(host=data["mysql"]["host"], user=data["mysql"]["user"], passwd=data["mysql"]["password"], db=data["mysql"]["database"], use_unicode=True, charset='utf8', init_command='SET NAMES UTF8')
+    conn = MySQLdb.connect(
+        host=data["mysql"]["host"],
+        user=data["mysql"]["user"],
+        passwd=data["mysql"]["password"],
+        db=data["mysql"]["database"],
+        use_unicode=True,
+        charset="utf8",
+        init_command="SET NAMES UTF8",
+    )
 
 
 if conn is None:
-    print("NO CONNECTION")
-    exit()
+    logging.error("NO CONNECTION")
+    sys.exit(1)
 
 
-def addToDb( id, props, conn, iter ):
-
+def addToDb(id, props, conn, iter):
     c = conn.cursor()
     records = []
 
     for prop in props:
-        records.append( ( id, prop ) )
+        records.append((id, prop))
 
-    c.executemany( "INSERT INTO `authorities` (`id`, `authority`) VALUES ( %s, %s )", records )
+    c.executemany(
+        "INSERT INTO `authorities` (`id`, `authority`) VALUES ( %s, %s )", records
+    )
 
-    if iter > 10000 :
+    if iter > 10000:
         conn.commit()
         iter = 0
-    else :
+    else:
         iter = iter + 1
 
     return iter
@@ -62,28 +79,33 @@ def addToDb( id, props, conn, iter ):
 if "authorities" in args:
     if args.authorities is not None:
         with open(args.authorities) as authorities_file:
-            csvreader = csv.reader(authorities_file, delimiter='\t')
+            csvreader = csv.reader(authorities_file, delimiter="\t")
             for row in csvreader:
                 authorities[row[2]] = 1
-                authtypes[row[2]] = [ row[0], row[1], row[2], row[3] ]
+                authtypes[row[2]] = [row[0], row[1], row[2], row[3]]
 
-#pp.pprint( authorities )
+# pp.pprint( authorities )
 
 if "dump" in args:
     if args.dump is not None:
-
         cur = conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS `run` (  `date` datetime DEFAULT CURRENT_TIMESTAMP, `name` VARCHAR(25), PRIMARY KEY (`date`, `name`) ) ;")
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS `run` (  `date` datetime DEFAULT CURRENT_TIMESTAMP, `name` VARCHAR(25), PRIMARY KEY (`date`, `name`) ) ;"
+        )
         cur.execute("CREATE INDEX IF NOT EXISTS idx_date ON run (date);")
         cur.execute("CREATE INDEX IF NOT EXISTS idx_name ON run (name);")
 
         cur.execute("DROP TABLE IF EXISTS `authorities`;")
-        cur.execute("CREATE TABLE IF NOT EXISTS `authorities` (  `id` VARCHAR(25), `authority` VARCHAR(25), PRIMARY KEY (`id`, `authority`) ) ;")
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS `authorities` (  `id` VARCHAR(25), `authority` VARCHAR(25), PRIMARY KEY (`id`, `authority`) ) ;"
+        )
         cur.execute("CREATE INDEX idx_id ON authorities (id);")
         cur.execute("CREATE INDEX idx_authorities ON authorities (authority);")
 
         cur.execute("DROP TABLE IF EXISTS `authtypes`;")
-        cur.execute("CREATE TABLE IF NOT EXISTS `authtypes` (  `name` VARCHAR(25), `desc` VARCHAR(125), `prop` VARCHAR(25), `authtype` int(2), PRIMARY KEY (`prop`) ) ;")
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS `authtypes` (  `name` VARCHAR(25), `desc` VARCHAR(125), `prop` VARCHAR(25), `authtype` int(2), PRIMARY KEY (`prop`) ) ;"
+        )
         cur.execute("CREATE INDEX IF NOT EXISTS `idx_name` ON authtypes (name);")
         cur.execute("CREATE INDEX IF NOT EXISTS `idx_type` ON authtypes (authtype);")
 
@@ -91,24 +113,32 @@ if "dump" in args:
 
         # Fill authtypes
         for prop in authtypes:
-            cur.execute( "INSERT INTO `authtypes` (`name`, `desc`, `prop`, `authtype`) VALUES ( %s, %s, %s, %s )", [ authtypes[prop][0], authtypes[prop][1], authtypes[prop][2], authtypes[prop][3] ] )
+            cur.execute(
+                "INSERT INTO `authtypes` (`name`, `desc`, `prop`, `authtype`) VALUES ( %s, %s, %s, %s )",
+                [
+                    authtypes[prop][0],
+                    authtypes[prop][1],
+                    authtypes[prop][2],
+                    authtypes[prop][3],
+                ],
+            )
 
         iter = 0
         id = None
-        with gzip.open(args.dump,'rt') as f:
+        with gzip.open(args.dump, "rt") as f:
             for line in f:
-                detectid = re.findall( r'\"type\":\"item\",\"id\":\"(Q\d+)\"', line )
-                if len( detectid ) > 0:
+                detectid = re.findall(r"\"type\":\"item\",\"id\":\"(Q\d+)\"", line)
+                if len(detectid) > 0:
                     id = detectid[0]
                     # print( id )
-                    listp = re.findall( r'\"(P\d+)\"', line )
+                    listp = re.findall(r"\"(P\d+)\"", line)
                     authp = []
                     for prop in listp:
                         if prop in authorities:
-                            authp.append( prop )
+                            authp.append(prop)
                     # pp.pprint( authp )
-                    if len( authp ) > 0 and id is not None :
-                        iter = addToDb( id, list(set(authp)), conn, iter )
+                    if len(authp) > 0 and id is not None:
+                        iter = addToDb(id, list(set(authp)), conn, iter)
                         id = None
 
         conn.commit()
